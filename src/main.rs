fcc782f90761d9d966d4303920f4a4f649265c50
Stephen Battista <stephen.battista@gmail.com>
2019-12-18 21:07:02 -0500

    Figured out witness takes way too long.

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,20 @@ fn random_vector (lenght:usize) -> Vec<usize>{
     answer
 }
 
-const SET_SIZE : usize = 10000;
+const SET_SIZE : usize = 100000;
 fn main() {
     let mut rng = rand::thread_rng();
     println!("Createing Vector of size {}", SET_SIZE);
     let set = random_vector(SET_SIZE);
     let target = set[rng.gen_range(0, set.len())];
     println!("Target is {}",target);
+    let mut acc = Accumulator::<Rsa2048, usize>::empty();
+    //println!("Set: {:?}",set);
     println!("Creating vector without target");
     let set2 : Vec<usize> = set.clone().into_iter().filter (|&x| x!=target).collect();
-    let mut acc = Accumulator::<Rsa2048, usize>::empty();
     let mut acc_2 = Accumulator::<Rsa2048, usize>::empty();
     let progress_bar = ProgressBar::new(set.len().try_into().unwrap());
-    progress_bar.set_prefix("Creating full accumlator");
+    progress_bar.set_prefix("Creating full accumlator :");
     progress_bar.set_style(
         ProgressStyle::default_bar()
             .template("{prefix} {wide_bar} {pos}/{len} {elapsed_precise} {eta_precise}"),
@@ -42,7 +43,7 @@ fn main() {
     progress_bar.finish();
     println!("Publish full set accumlator {:?}", acc);
     let progress_bar = ProgressBar::new(set2.len().try_into().unwrap());
-    progress_bar.set_prefix("Creating accumlator without target");
+    progress_bar.set_prefix("Creating accumlator without target :");
     progress_bar.set_style(
         ProgressStyle::default_bar()
             .template("{prefix} {wide_bar} {pos}/{len} {elapsed_precise} {eta_precise}"),
@@ -52,6 +53,7 @@ fn main() {
         progress_bar.inc(1);
     }
     progress_bar.finish();
+    //println!("Publish partial set accumlator {:?}", acc_2);
     println!("Creating proof");
     let (_acc_new, proof)  = acc_2.add_with_proof(&[target]);
     println!("Sending proof {:?}", proof);
@@ -59,30 +61,25 @@ fn main() {
     println!("Answer {}",answer);
 
 
-   let witness_multiple = Witness(acc.clone());
-   for  (elem,target_witness) in &witness_multiple.compute_individual_witnesses(&[target]){
-       if *elem == target {
-           println!("Witness_multiple {:?}",witness_multiple);
-           println!("Witness: {:?}", target_witness);
-           println!("Deleteing target");
-           acc = acc.clone().delete(&[(target,target_witness.clone())]).unwrap();
-       }
-    }
-   println!("Creating proof");
-   let (_acc_new, proof)  = acc.clone().add_with_proof(&[target]);
-   println!("Sending proof {:?}", proof);
-   let answer = acc.verify_membership(&target,&proof);
-   println!("Answer {}",answer);
+    /*println!("Creating subset witness");
+    let spinner = ProgressBar::new_spinner();
+    spinner.set_prefix("Creating Witness:");
+    spinner
+        .set_style(ProgressStyle::default_bar().template("{prefix} {spinner} {elapsed_precise}"));
+    spinner.enable_steady_tick(100);
+    let witness = Witness(Accumulator::<Rsa2048, usize>::empty()).compute_subset_witness(&set,&[target]).unwrap();
+    spinner.finish();
+
+
+    println!("Deleteing target");
+    acc = acc.clone().delete(&[(target,witness.clone())]).unwrap();
+
+
+    println!("Creating proof");
+    let (acc_new, proof)  = acc.clone().add_with_proof(&[target]);
+
+    println!("Sending proof {:?}", proof);
+    let answer = acc_new.verify_membership(&target,&proof);
+    println!("Verification is {}",answer); */
 
 }
-
-
-
-//println!("Acc: {:?}\nProof:{:?}",acc,proof);
-
-// A network participant who sees (acc, proof, and ["dog", "cat"]) can verify that the update
-// was formed correctly ...
-//assert!(acc.verify_membership_batch(&set, &proof));
-
-// ... and trying to verify something that has not been accumulated will fail.
-//assert!(!acc.verify_membership(&"cow", &proof));=
